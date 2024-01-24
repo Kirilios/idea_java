4b0cfc93fcc48e8ec3e0cd48bd29374ccf5d16fa
Kirilios <kirilios17@mail.ru>
2024-01-24 22:25:22 +0300

    added hw tasks2

--- OOPSeminars/Seminar5/Service/StudentGroupService.java
+++ OOPSeminars/Seminar5/Service/StudentGroupService.java
@@ -7,7 +7,7 @@ import OOPSeminars.Seminar5.Data.User;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class StudentGroupService {
     private List<StudentGroup> studentGroups;
